68019c5168781038d37ee41fc56d028cc927e8a0
ZygimantasL <129725364+ZygimantasL@users.noreply.github.com>
2023-06-06 11:07:35 +0300

    list.json

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
-cons ulDOM = document.querySelector('ul');
+//import * as allData from '../data/list.js';
+//import {data } from './data/list.js';
+
+const ulDOM = document.querySelector('ul');
 
 const data = [
     {id: 1, text: 'Labas'},
@@ -7,3 +10,25 @@ const data = [
 ]
 
 ulDOM.innerHTML = ''
+
+// LOL
+
+
+const data = fetch('https://front-end-by-rimantas.github.io/44-grupe-fetch/data/users.json')
+    .then((response) => {
+        return response.json();
+    })
+    .then((data) => {
+        for (const user of data) {
+            console.log(user);
+        }
+    });
+
+
+// let HTML = '';
+
+// for (const item of data) {
+//     HTML += `<li>${item.id}) ${item.text}</li>`;
+// }
+
+// ulDOM.innerHTML = HTML;
